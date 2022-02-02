ff87a1e4f08567e68a01b8c2f6f0cdfb17d904a9
edgar palencia <edgaralejandropalencia123@gmail.com>
2022-02-02 12:56:01 -0300

    prueba ejercicios 2

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -7,7 +7,7 @@ const nuevaString = "edgar";
 const nuevoNum = 4;
 
 // Crea una variable booleana:
-const nuevoBool = trueed;
+const nuevoBool = true;
 
 // Resuelve el siguiente problema matemático:
 const nuevaResta = 10 - 5 === 5;
